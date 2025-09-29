a3d4404006c844c76d9ad74a33c881d56a7731f8
kkkosei <kou505011@gmail.com>
2025-09-29 22:37:44 +0900

    簡易的なルーティングの設定完了

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+// LoginPage.jsx
+function LoginPage() {
+  return <h1>Login Page</h1>;
+}
+export default LoginPage;
---
+++ client/src/pages/StudyLogPage.jsx
@@ -0,0 +1,4 @@
+function StudyLogPage() {
+  return <h1>Study Log Page</h1>;
+}
+export default StudyLogPage;
